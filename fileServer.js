a89550972c88ab2fcda0e122caebf810f561f144
jerripat <72359888+jerripat@users.noreply.github.com>
2021-11-13 11:38:59 -0600

    updates

---
+++ fileServer.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function (req,res) {
+fs.readFile('index.html',function(err,data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+    })
+})
+.listen(8080);
+
+console.log('Server is listening on port 8080');
+
