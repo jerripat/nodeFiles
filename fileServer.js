58a3e18ce32683a6402ccc926dfa51fb466f40f4
jerripat <72359888+jerripat@users.noreply.github.com>
2021-11-13 18:28:12 -0600

    updates

--- fileServer.js
+++ fileServer.js
@@ -1,26 +1,26 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 
-http.createServer(function (req,res) {
+http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
-   var filename = "." + q.pathname;
-   if(filename == './'){filename = './index';}
-   filename = filename + ".html";
-;    fs.readFile(filename,function(err,data) {
-        if (err){
-            res.writeHead(404,{'Content-Type': 'text/html'});
+    var filename = "." + q.pathname;
+    if (filename == './') { filename = './index'; }
+    filename = filename + ".html";;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
             return res.end("404 Not Found");
         }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    console.log('...incoming Request: ' + req.url);
-    res.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        console.log('...incoming Request: ' + req.url);
+        res.end();
     })
-}).listen(8080);
+}).listen(PORT);
 
 
 
 console.log('Server is listening on port 8080');
-
